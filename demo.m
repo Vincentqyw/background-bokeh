d4c3f80908bfca3d62f443a70ec281cfbd774937
Vincent Qin <qin123yw@163.com>
2018-01-10 00:47:29 +0800

    first commit

---
+++ demo.m
@@ -0,0 +1,7 @@
+% Color image is the central view of a light field (shot by ILLUM).
+% The rawdepth is obtained by a depth estimation method based on light field cameras.
+
+% more info: Vincent Qin (https://github.com/Vincentqyw) 
+depth       =imread('rawdepth.png');
+color_image =imread('color_image.png');
+[out,mask]=imageBokeh(color_image,rawdepth);
---
+++ imageBokeh.m
@@ -0,0 +1,96 @@
+function [out,mask]=imageBokeh(color_image,depth)
+
+% input 
+% @color_image  : color image
+% @depth        : depth map
+
+% output
+% @out          : Bokeh image
+% @mask         : binary mask of foreground  
+
+% NOTE: This script is written by ShreyasSkandan AND then 
+% REVISED by Vincent Qin (https://github.com/Vincentqyw)
+% More information: https://github.com/ShreyasSkandan/stereo-background-blur
+
+
+
+RECT_LEFT_CLR=color_image;
+DEPTH=single(depth);
+
+slope_threshold = 10;
+depth_threshold = 8;
+% For K-Means depth segmentation algorithm
+nDepths = 2; % nDepths classes
+
+% Created the background image - blurred image
+H = fspecial('disk',10);
+blurredImage = imfilter(RECT_LEFT_CLR,H,'replicate');
+
+% Unroll both colour and blurred images into vectors
+colour_image_vec = reshape(RECT_LEFT_CLR,[size(DEPTH,1)*size(DEPTH,2),3]);
+blurred_image_vec = reshape(blurredImage,[size(DEPTH,1)*size(DEPTH,2),3]);
+
+% Use K-Means to segment out the depth image into
+ab = reshape(DEPTH,[size(DEPTH,1)*size(DEPTH,2),1]);
+[cluster_idx, cluster_center] = kmeans(ab,nDepths,'distance','sqEuclidean','Replicates',3);
+pixel_labels = reshape(cluster_idx,[size(DEPTH,1),size(DEPTH,2)]);
+% save('kmeansresult.mat',pixel_labels);
+% figure,
+% imshow(pixel_labels,[]);
+% segdata = load('kmeansresult.mat');
+% pixel_labels = segdata.pixel_labels;
+
+% Create a mask of all pixels at the prescribed depth (usually the max
+% disparity -> minimum depth)
+mask = zeros(size(DEPTH,1)*size(DEPTH,2),1);
+indices = find(pixel_labels == max(max(pixel_labels)));
+mask(indices) = 1;
+mask = reshape(mask,[size(DEPTH,1),size(DEPTH,2)]);
+
+% Perform a few morphological operations to create a more uniform mask
+mask = imfill(mask,'holes');
+mask = bwmorph(mask,'clean');
+mask = bwmorph(mask,'open',11);
+mask = bwmorph(mask,'close',10);
+% mask = bwmorph(mask,'thicken',3);
+
+% Extract the largest connected component
+mask = bwareafilt(mask, 1, 'largest');
+mask = imfill(mask,'holes');
+mask = bwmorph(mask,'spur',12);
+mask = bwmorph(mask,'thin',2);
+
+use_new_mask=0;
+if use_new_mask
+% Create a distance transform matrix from the mask outward
+D = bwdist(mask);
+D(D>depth_threshold) = slope_threshold;
+% Standardize the distance transform to create a gradient
+maxD = max(max(D));
+minD = min(min(D));
+mean = mean2(D);
+normDistTransVec = (D - minD)/(maxD-minD);
+%newmask = ones(size(DEPTH,1),size(DEPTH,2));
+%newmask(row_start-padding:row_end+padding,col_start-padding:col_end+padding) = normDistTransVec;
+%normDistTransVec = newmask;
+newmask =normDistTransVec;
+figure,
+imagesc(newmask);
+
+Evec = reshape(normDistTransVec,[size(normDistTransVec,1)*size(normDistTransVec,2),1]);
+gradient = [Evec,Evec,Evec];
+
+% Apply the gradient onto the image
+resultingImageVec = double(gradient).*double(blurred_image_vec) + double(1-gradient).*double(colour_image_vec);
+resImage = reshape(resultingImageVec,[size(DEPTH,1),size(DEPTH,2),3]);
+end
+% out = uint8(resImage);
+
+% mask_3_channels= uint8(repmat(mask,[1 1 3]));
+mask_3_channels= uint8([mask(:),mask(:),mask(:)]);
+res= (colour_image_vec).*mask_3_channels+(1-mask_3_channels).*blurred_image_vec;
+% figure;imshow(uint8(repmat(mask,[1 1 3])).*color_image);
+out = reshape(res,[size(DEPTH,1),size(DEPTH,2),3]);
+
+figure;imshow(out);
+
